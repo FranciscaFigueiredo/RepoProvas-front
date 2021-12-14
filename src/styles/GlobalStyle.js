2eec294923780b9f4493ac31dde9e81a5cbf890e
Francisca <fraan928@gmail.com>
2021-12-13 23:34:44 -0300

    Project structure

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		text-decoration: none;
+		border: none;
+
+		font-family: 'Oswald', sans-serif;
+	}
+
+	body {
+		background-color: #8c11be;
+		font-family: 'Raleway', sans-serif;
+	}
+`;
+ 
+export default GlobalStyle;
